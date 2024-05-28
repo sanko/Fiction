4725436258e6922c41e17a6b1e3664eb37fd8bad
Sanko Robinson <sanko@cpan.org>
2024-05-28 16:25:59 -0400

    Move more examples to the cookbook

--- eg/win32_msgbox.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-use lib '../lib', '../blib/arch', '../blib/lib';
-use Affix;
-$|++;
-#
-CORE::say 'MessageBoxA(...) = ' .
-    wrap( 'C:\Windows\System32\user32.dll', 'MessageBoxA', [ UInt, String, String, UInt ] => Int )
-    ->( 0, 'JAPH!', 'Hello, World', 0 );
